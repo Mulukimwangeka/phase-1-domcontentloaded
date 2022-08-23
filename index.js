66dafcc3928bf9984734465532346d2bd70b3d4a
mulukimwangeka <mwangekaruth@gmail.com>
2022-08-23 12:34:52 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    let ptext = document.getElementById("text");
+    ptext.textContent="This is really cool!"
+  });
